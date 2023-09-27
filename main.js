a5238150b6c06dbb456ebb304655c28665152ba2
Ghassanooooo <ghassanaldarwish@yahoo.com>
2023-09-27 16:00:36 +0200

    up

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 const root = document.querySelector("#root");
+const download = document.querySelector(".download");
+
+function onDownload(filename, text) {
+  var element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+  );
+  element.setAttribute("download", filename);
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
 
 window.addEventListener("DOMContentLoaded", () => {
   fetch("data.md")
     .then((res) => res.text())
     .then((data) => {
+      download.addEventListener("click", () => {
+        onDownload("doc.md", data);
+        download.style.transform = "scale(0.94)";
+        download.innerText = "Downloaded!";
+        setTimeout(() => {
+          download.style.transform = "scale(1)";
+          download.innerText = "Download";
+        }, 1000);
+      });
       root.innerHTML = marked.parse(data);
       hljs.highlightAll();
       const code = document.querySelectorAll("pre code");
